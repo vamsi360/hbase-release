9a7f37cee1314aaffb1efe0923ac0a5280a6f1cb
Raja Aluri <raja@cmbasics.com>
2014-02-03 09:10:29 -0800

    WINPKG CHANGES: These are all the winpkg related changes from monarch.
        We are bringing in as one commit. This is a squashed commit of the following
        commits. This will skew the author information, but for the real author
        information, please use the commits that are showing in as cherry-picks
        from monarch repo.
    
    3aa55cb raja@cmbasics.com Adding hbase packaging changes for winpkg profile
    ab5b141 raja@cmbasics.com Adding hbase packaging changes for winpkg profile
    ca3e1fe raja@cmbasics.com ONPREM-377 wrong value set for hbase.rootdir in HA setup causes master to not start.
    1e89fa0 contact@rusanu.com HADOOP-699 replace servicehost.exe binaries
    e8f4264 enis@apache.org Added -XX:+ForceTimeHighResolution by default for MSI installation template
    c3bcbff enis@apache.org Do not define JAVA_HOME in hbase-env.cmd template
    4485038 enis@apache.org HBASE-118 Pick up new hadoop conf dir from HADOOP_HOME/etc/hadoop
    e99612b raja@cmbasics.com Moving windows resources and hbase-assembly module
    
    Conflicts:
    	hbase-assembly/src/main/assembly/hadoop-one-compat.xml

---
+++ hbase-assembly/src/packages/win/scripts/install.ps1
@@ -0,0 +1,269 @@
+### Licensed to the Apache Software Foundation (ASF) under one or more
+### contributor license agreements.  See the NOTICE file distributed with
+### this work for additional information regarding copyright ownership.
+### The ASF licenses this file to You under the Apache License, Version 2.0
+### (the "License"); you may not use this file except in compliance with
+### the License.  You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+
+param(
+	[String]
+    [Parameter( ParameterSetName='UsernamePassword', Position=0, Mandatory=$true )]
+    [Parameter( ParameterSetName='UsernamePasswordBase64', Position=0, Mandatory=$true )]
+    $username,
+    [String]
+    [Parameter( ParameterSetName='UsernamePassword', Position=1, Mandatory=$true )]
+    $password,
+    [String]
+    [Parameter( ParameterSetName='UsernamePasswordBase64', Position=1, Mandatory=$true )]
+    $passwordBase64,
+    [Parameter( ParameterSetName='CredentialFilePath', Mandatory=$true )]
+    $credentialFilePath,
+	$hbaseRole
+	)
+
+function Main( $scriptDir )
+{
+	$HDP_INSTALL_PATH, $HDP_RESOURCES_DIR = Initialize-InstallationEnv $scriptDir "hbase-@version@.winpkg.log"
+Test-JavaHome
+
+	### $hbaseInstallDir: the directory that contains the appliation, after unzipping
+	$hbaseInstallDir = Join-Path "$ENV:HADOOP_NODE_INSTALL_ROOT" "hbase-@version@"
+	$hbaseInstallBin = Join-Path "$hbaseInstallDir" "bin"
+
+	Write-Log "HbaseInstallDir: $hbaseInstallDir"
+	Write-Log "HbaseInstallBin: $hbaseInstallBin"
+
+	###
+	### Create the Credential object from the given username and password or the provided credentials file
+	###
+    $serviceCredential = Get-HadoopUserCredentials -credentialsHash @{"username" = $username; "password" = $password; `
+        "passwordBase64" = $passwordBase64; "credentialFilePath" = $credentialFilePath}
+    $username = $serviceCredential.UserName
+	Write-Log "Username: $username"
+	Write-Log "CredentialFilePath: $credentialFilePath"
+
+	Write-Log "Ensuring elevated user"
+
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent( ) )
+	if ( -not ($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ) ) )
+	{
+		Write-Log "install script must be run elevated" "Failure"
+		exit 1
+	}
+
+	if( -not (Test-Path $ENV:JAVA_HOME\bin\java.exe))
+	{
+		Write-Log "JAVA_HOME not set properly; $ENV:JAVA_HOME\bin\java.exe does not exist" "Failure"
+		exit 1
+	}
+
+	###
+	### Begin install
+	###
+	Write-Log "Installing Apache Hadoop Hbase hbase-@version@ to $ENV:HADOOP_NODE_INSTALL_ROOT"
+
+	if( $username -eq $null )
+	{
+		Write-Log "Username cannot be empty" "Failure"
+		exit 1
+	}
+        $hbaseRole = " "
+	if( $ENV:IS_HBASE_MASTER -eq "yes")
+	{
+		$hbaseRole =  "master"
+	}
+
+	if( $ENV:IS_HBASE_REGIONSERVER -eq "yes" )
+	{
+		$hbaseRole = $hbaseRole+" "+"regionserver"
+	}
+
+	if( $ENV:IS_HBASE_MASTER -eq "yes" -or $ENV:IS_HBASE_REGIONSERVER -eq "yes")
+	{
+		$hbaseRole = $hbaseRole+" "+"rest"
+		$hbaseRole = $hbaseRole+" "+"thrift"
+		$hbaseRole = $hbaseRole+" "+"thrift2"
+  }
+
+  $hbaseRole = $hbaseRole.Trim()
+
+	###
+	###  Unzip Hbase distribution from compressed archive
+	###
+
+	Write-Log "Extracting Hbase archive into $hbaseInstallDir"
+	$unzipExpr = "$ENV:WINPKG_BIN\winpkg.ps1 `"$HDP_RESOURCES_DIR\hbase-@version@.zip`" utils unzip `"$ENV:HADOOP_NODE_INSTALL_ROOT`""
+	Invoke-Ps $unzipExpr
+
+	$xcopy_cmd = "xcopy /EIYF `"$HDP_INSTALL_PATH\..\template`" `"$hbaseInstallDir`""
+	Invoke-Cmd $xcopy_cmd
+
+	###
+	### Grant Hbase user access to HADOOP_INSTALL_DIR and HDFS Root
+	###
+	$cmd = "icacls `"$hbaseInstallDir`" /grant `"${username}`":(OI)(CI)F"
+	Invoke-Cmd $cmd
+
+	###
+	### Create Hbase Windows Services and grant user ACLS to start/stop
+	###
+    if ($hbaseRole){
+        Write-Log "HbaseRole: $hbaseRole"
+
+        Write-Log "Installing services $hbaseRole"
+
+        foreach( $service in $hbaseRole -Split('\s+'))
+        {
+            try
+            {
+                Write-Log "Creating service $service as $hbaseInstallBin\$service.exe"
+                Copy-Item "$HDP_RESOURCES_DIR\serviceHost.exe" "$hbaseInstallBin\$service.exe" -Force
+
+                #serviceHost.exe will write to this log but does not create it
+                #Creating the event log needs to be done from an elevated process, so we do it here
+                if( -not ([Diagnostics.EventLog]::SourceExists( "$service" )))
+                {
+                    [Diagnostics.EventLog]::CreateEventSource( "$service", "" )
+                }
+
+                Write-Log( "Adding service $service" )
+                $s = New-Service -Name "$service" -BinaryPathName "$hbaseInstallBin\$service.exe" -Credential $serviceCredential -DisplayName "Apache Hadoop Hbase $service"
+
+                $cmd="$ENV:WINDIR\system32\sc.exe failure $service reset= 30 actions= restart/5000"
+                Invoke-Cmd $cmd
+				$disabled_services = 'rest','thrift','thrift2'
+				if ($disabled_services -match $service)
+				{
+					$cmd="$ENV:WINDIR\system32\sc.exe config $service start= disabled"
+				}
+                else
+				{
+					$cmd="$ENV:WINDIR\system32\sc.exe config $service start= demand"
+				}
+                Invoke-Cmd $cmd
+
+                Set-ServiceAcl $service
+            }
+            catch [Exception]
+            {
+                Write-Log $_.Exception.Message $_
+            }
+        ###
+        ### Setup HBASE service config
+        ###
+
+            Write-Log "Copying configuration for $hbaseRole"
+
+            Write-Log "Creating service config ${hbaseInstallBin}\${service}.xml"
+            $cmd = "$hbaseInstallBin\hbase.cmd --service $service start > `"$hbaseInstallBin\$service.xml`""
+            Invoke-Cmd $cmd
+        }
+    }
+    else{
+        Write-Log "No Hbase roles are defined. Not adding any services"
+    }
+	###
+	### ACL Hbase logs directory such that machine users can write to it
+	###
+	if( -not (Test-Path "$hbaseInstallDir\logs"))
+	{
+		Write-Log "Creating Hbase logs folder"
+		$cmd = "mkdir `"$hbaseInstallDir\logs`""
+		Invoke-Cmd $cmd
+	}
+	Write-Log "Giving Users group full permissions on the Hbase logs folder"
+	$cmd = "icacls `"$hbaseInstallDir\logs`" /grant Users:(OI)(CI)F"
+	Invoke-Cmd $cmd
+
+        ###
+        ### Apply core-site.xml configuration changes
+        ###
+        $hbaseSiteXmlFile = Join-Path $hbaseInstallDir "conf\hbase-site.xml"
+
+        Write-Log "Updating hbase config file $hbaseSiteXmlFile"
+
+        $hbaseRootDir = "hdfs://${ENV:NAMENODE_HOST}:8020/apps/hbase/data"
+
+        if ($ENV:HA -ieq "yes") {
+            $hbaseRootDir = "hdfs://${ENV:HA_CLUSTER_NAME}/apps/hbase/data"
+        }
+
+        $zookeeper_quorum = ($ENV:ZOOKEEPER_HOSTS.Split(",") | foreach { $_.Trim() + ":2181"}) -join ","
+        UpdateXmlConfig $hbaseSiteXmlFile @{
+        "hbase.rootdir" = "$hbaseRootDir"
+        "hbase.zookeeper.quorum" = "$zookeeper_quorum"}
+
+	Write-Log "Installation of Hbase Core complete"
+}
+### Helper routing that converts a $null object to nothing. Otherwise, iterating over
+### a $null object with foreach results in a loop with one $null element.
+function empty-null($obj)
+{
+   if ($obj -ne $null) { $obj }
+}
+
+
+
+    ### Helper routine that updates the given fileName XML file with the given
+    ### key/value configuration values. The XML file is expected to be in the
+    ### Hadoop format. For example:
+    ### <configuration>
+    ###   <property>
+    ###     <name.../><value.../>
+    ###   </property>
+    ### </configuration>
+function UpdateXmlConfig(
+    [string]
+    [parameter( Position=0, Mandatory=$true )]
+    $fileName,
+    [hashtable]
+    [parameter( Position=1 )]
+    $config = @{} )
+{
+    $xml = [xml] (Get-Content $fileName)
+
+    foreach( $key in empty-null $config.Keys )
+    {
+        $value = $config[$key]
+        $found = $False
+        $xml.SelectNodes('/configuration/property') | ? { $_.name -eq $key } | % { $_.value = $value; $found = $True }
+        if ( -not $found )
+        {
+            $xml["configuration"].AppendChild($xml.CreateWhitespace("`r`n  ")) | Out-Null
+            $newItem = $xml.CreateElement("property")
+            $newItem.AppendChild($xml.CreateWhitespace("`r`n    ")) | Out-Null
+            $newItem.AppendChild($xml.CreateElement("name")) | Out-Null
+            $newItem.AppendChild($xml.CreateWhitespace("`r`n    ")) | Out-Null
+            $newItem.AppendChild($xml.CreateElement("value")) | Out-Null
+            $newItem.AppendChild($xml.CreateWhitespace("`r`n  ")) | Out-Null
+            $newItem.name = $key
+            $newItem.value = $value
+            $xml["configuration"].AppendChild($newItem) | Out-Null
+            $xml["configuration"].AppendChild($xml.CreateWhitespace("`r`n")) | Out-Null
+        }
+    }
+    $xml.Save($fileName)
+    $xml.ReleasePath
+}
+
+try
+{
+	$scriptDir = Resolve-Path (Split-Path $MyInvocation.MyCommand.Path)
+	$utilsModule = Import-Module -Name "$scriptDir\..\resources\Winpkg.Utils.psm1" -ArgumentList ("HBASE") -PassThru
+	Main $scriptDir
+}
+finally
+{
+	if( $utilsModule -ne $null )
+	{
+		Remove-Module $utilsModule
+	}
+}
