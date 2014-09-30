9f809a2eceadeb7b64091bf0fb95f647ce9e519b
Enis Soztutar <enis@apache.org>
2014-09-30 11:33:58 -0700

    HBASE-12099 TestScannerModel fails if using jackson 1.9.13 (Esteban Gutierrez)

--- hbase-server/src/main/java/org/apache/hadoop/hbase/rest/model/ScannerModel.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/rest/model/ScannerModel.java
@@ -646,7 +646,7 @@ public class ScannerModel implements ProtobufMessageHandler, Serializable {
     return columns;
   }
   
-  @XmlElement(name="label")
+  @XmlElement(name="labels")
   public List<String> getLabels() {
     return labels;
   }
