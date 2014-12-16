9e4ea9350f634f089641c82b9c6ca1f4c8b49b18
Jeffrey Zhong <jeffreyz@apache.org>
2014-12-15 18:44:05 -0800

    BUG-29025: Windows incompatibility with unicode (Chinese characters)
    Conflicts:
    	hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/provider/producer/PlainTextMessageBodyProducer.java

--- hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/provider/producer/PlainTextMessageBodyProducer.java
+++ hbase-rest/src/main/java/org/apache/hadoop/hbase/rest/provider/producer/PlainTextMessageBodyProducer.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.nio.charset.Charset;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
@@ -57,7 +58,7 @@ public class PlainTextMessageBodyProducer
   @Override
   public long getSize(Object object, Class<?> type, Type genericType,
       Annotation[] annotations, MediaType mediaType) {
-    byte[] bytes = object.toString().getBytes(); 
+    byte[] bytes = object.toString().getBytes(Charset.forName("UTF-8"));
     buffer.set(bytes);
     return bytes.length;
   }
