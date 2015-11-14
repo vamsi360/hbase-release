60931c35aa40280e31ce582dbb30557acc85feb7
stack <stack@apache.org>
2015-11-14 09:07:39 -0800

    HBASE-14798 NPE reporting server load causes regionserver abort; causes TestAcidGuarantee to fail

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/DefaultStoreFileManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/DefaultStoreFileManager.java
@@ -73,6 +73,7 @@ class DefaultStoreFileManager implements StoreFileManager {
 
   @Override
   public final Collection<StoreFile> getStorefiles() {
+    // TODO: I can return a null list of StoreFiles? That'll mess up clients. St.Ack 20151111
     return storefiles;
   }
 
