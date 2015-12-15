20393eb06b3723abb05304cc8dabc566a7260ad5
Elliott Clark <eclark@apache.org>
2015-12-15 15:35:16 -0800

    HBASE-14983 Create metrics for per block type hit/miss ratios
    Summary: Missing a root index block is worse than missing a data block. We should know the difference
    
    Test Plan: Tested on a local instance. All numbers looked reasonable.
    
    Differential Revision: https://reviews.facebook.net/D55563
    
    Conflicts:
    	hbase-hadoop-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsRegionServerSource.java
    	hbase-hadoop2-compat/src/main/java/org/apache/hadoop/hbase/regionserver/MetricsRegionServerSourceImpl.java
    	hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/CacheStats.java
    	hbase-server/src/test/java/org/apache/hadoop/hbase/io/hfile/TestCombinedBlockCache.java

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileBlockIndex.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileBlockIndex.java
@@ -979,7 +979,7 @@ public class HFileBlockIndex {
       if (cacheConf != null) {
         HFileBlock blockForCaching = blockWriter.getBlockForCaching(cacheConf);
         cacheConf.getBlockCache().cacheBlock(new BlockCacheKey(nameForCaching,
-          beginOffset), blockForCaching);
+          beginOffset, true, blockForCaching.getBlockType()), blockForCaching);
       }
 
       // Add intermediate index block size
