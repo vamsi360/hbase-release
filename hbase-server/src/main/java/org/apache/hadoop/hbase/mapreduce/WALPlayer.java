1862c64707c91a9c3594a3b632e2979445a1fcf5
tedyu <yuzhihong@gmail.com>
2016-05-11 09:46:04 -0700

    HBASE-15752 Addendum with log on value for tmpjars

--- hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/WALPlayer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/WALPlayer.java
@@ -321,6 +321,7 @@ public class WALPlayer extends Configured implements Tool {
     String codecCls = WALCellCodec.getWALCellCodecClass(conf);
     try {
       TableMapReduceUtil.addDependencyJars(conf, Class.forName(codecCls));
+      LOG.debug("tmpjars: " + conf.get("tmpjars"));
     } catch (Exception e) {
       throw new IOException("Cannot determine wal codec class " + codecCls, e);
     }
