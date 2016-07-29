7d003738d9418d9ceea7b156f73c1fbff4ca73a7
Deokwoo Han <ithenine@gmail.com>
2016-07-29 11:07:51 +0900

    BUG-63624 HBASE-16284 Unauthorized client can shutdown the cluster
    (cherry-picked: 1b5f8c7)
    
    Change-Id: Ic45561a11384e1aff8020c0afd880464c30432b1

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -2379,13 +2379,9 @@ public class HMaster extends HRegionServer implements MasterServices, Server {
     return rsFatals;
   }
 
-  public void shutdown() {
+  public void shutdown() throws IOException {
     if (cpHost != null) {
-      try {
-        cpHost.preShutdown();
-      } catch (IOException ioe) {
-        LOG.error("Error call master coprocessor preShutdown()", ioe);
-      }
+      cpHost.preShutdown();
     }
 
     if (this.serverManager != null) {
@@ -2400,13 +2396,9 @@ public class HMaster extends HRegionServer implements MasterServices, Server {
     }
   }
 
-  public void stopMaster() {
+  public void stopMaster() throws IOException {
     if (cpHost != null) {
-      try {
-        cpHost.preStopMaster();
-      } catch (IOException ioe) {
-        LOG.error("Error call master coprocessor preStopMaster()", ioe);
-      }
+      cpHost.preStopMaster();
     }
     stop("Stopped by " + Thread.currentThread().getName());
   }
