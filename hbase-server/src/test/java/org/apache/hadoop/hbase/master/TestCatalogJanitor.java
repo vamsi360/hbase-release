0508999ed6c5942194585cbe693f7d53c5ced368
Matteo Bertozzi <matteo.bertozzi@cloudera.com>
2016-09-26 12:58:04 -0700

    HBASE-16649 Truncate table with splits preserved can cause both data loss and truncated data appeared again
    Change-Id: Ifc517aa81047a6542551c767a62952ecc9393b7e

--- hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestCatalogJanitor.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/master/TestCatalogJanitor.java
@@ -218,10 +218,12 @@ public class TestCatalogJanitor {
   class MockMasterServices implements MasterServices {
     private final MasterFileSystem mfs;
     private final AssignmentManager asm;
+    private final ServerManager sm;
 
     MockMasterServices(final Server server) throws IOException {
       this.mfs = new MasterFileSystem(server, this);
       this.asm = Mockito.mock(AssignmentManager.class);
+      this.sm = Mockito.mock(ServerManager.class);
     }
 
     @Override
@@ -288,7 +290,7 @@ public class TestCatalogJanitor {
 
     @Override
     public ServerManager getServerManager() {
-      return null;
+      return sm;
     }
 
     @Override
