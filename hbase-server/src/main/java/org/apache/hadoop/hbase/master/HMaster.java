joshelser/hbase

HBASE-16999 Implement master and regionserver synchronization of quota state
* Implement the RegionServer reading violation from the quota table
* Implement the Master reporting violations to the quota table
* RegionServers need to track its enforced policies

hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -126,6 +126,7 @@ import org.apache.hadoop.hbase.procedure2.store.wal.WALProcedureStore;
 import org.apache.hadoop.hbase.quotas.MasterQuotaManager;
 import org.apache.hadoop.hbase.quotas.QuotaObserverChore;
 import org.apache.hadoop.hbase.quotas.SpaceQuotaViolationNotifier;
+import org.apache.hadoop.hbase.quotas.SpaceQuotaViolationNotifierFactory;
 import org.apache.hadoop.hbase.quotas.SpaceQuotaViolationNotifierForTest;
 import org.apache.hadoop.hbase.regionserver.DefaultStoreEngine;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
@@ -914,7 +915,10 @@ public class HMaster extends HRegionServer implements MasterServices {
   }
 
   SpaceQuotaViolationNotifier createQuotaViolationNotifier() {
-    return new SpaceQuotaViolationNotifierForTest();
+    SpaceQuotaViolationNotifier notifier =
+        SpaceQuotaViolationNotifierFactory.getInstance().create(getConfiguration());
+    notifier.initialize(getClusterConnection());
+    return notifier;
   }
 
   boolean isCatalogJanitorEnabled() {

vamsi360/hbase-release

Change-Id: I2b0abb7f520138649019c3599d788eb4c1bd70e0

hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -157,6 +157,7 @@ import org.apache.hadoop.hbase.quotas.MasterQuotaManager;
 import org.apache.hadoop.hbase.quotas.QuotaObserverChore;
 import org.apache.hadoop.hbase.quotas.RegionStateListener;
 import org.apache.hadoop.hbase.quotas.SpaceQuotaViolationNotifier;
+import org.apache.hadoop.hbase.quotas.SpaceQuotaViolationNotifierFactory;
 import org.apache.hadoop.hbase.quotas.SpaceQuotaViolationNotifierForTest;
 import org.apache.hadoop.hbase.regionserver.DefaultStoreEngine;
 import org.apache.hadoop.hbase.regionserver.HRegionServer;
@@ -1077,7 +1078,10 @@ public class HMaster extends HRegionServer implements MasterServices, Server {
   }
 
   SpaceQuotaViolationNotifier createQuotaViolationNotifier() {
-    return new SpaceQuotaViolationNotifierForTest();
+    SpaceQuotaViolationNotifier notifier =
+        SpaceQuotaViolationNotifierFactory.getInstance().create(getConfiguration());
+    notifier.initialize(getConnection());
+    return notifier;
   }
 
   boolean isCatalogJanitorEnabled() {