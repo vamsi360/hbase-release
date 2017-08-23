makaidong/hbase

HBASE-17978 Ensure superusers can circumvent actions restricted by space quota violations

hbase-server/src/test/java/org/apache/hadoop/hbase/quotas/SpaceQuotaHelperForTests.java
@@ -110,6 +110,18 @@ public class SpaceQuotaHelperForTests {
     }
   }
 
+  QuotaSettings getTableSpaceQuota(Connection conn, TableName tn) throws IOException {
+    try (QuotaRetriever scanner = QuotaRetriever.open(
+        conn.getConfiguration(), new QuotaFilter().setTableFilter(tn.getNameAsString()))) {
+      for (QuotaSettings setting : scanner) {
+        if (setting.getTableName().equals(tn) && setting.getQuotaType() == QuotaType.SPACE) {
+          return setting;
+        }
+      }
+      return null;
+    }
+  }
+
   /**
    * Waits 30seconds for the HBase quota table to exist.
    */
@@ -130,7 +142,10 @@ public class SpaceQuotaHelperForTests {
   }
 
   void writeData(TableName tn, long sizeInBytes) throws IOException {
-    final Connection conn = testUtil.getConnection();
+    writeData(testUtil.getConnection(), tn, sizeInBytes);
+  }
+
+  void writeData(Connection conn, TableName tn, long sizeInBytes) throws IOException {
     final Table table = conn.getTable(tn);
     try {
       List<Put> updates = new ArrayList<>();
@@ -226,8 +241,16 @@ public class SpaceQuotaHelperForTests {
     return createTableWithRegions(NamespaceDescriptor.DEFAULT_NAMESPACE_NAME_STR, numRegions);
   }
 
+  TableName createTableWithRegions(Admin admin, int numRegions) throws Exception {
+    return createTableWithRegions(
+        testUtil.getAdmin(), NamespaceDescriptor.DEFAULT_NAMESPACE_NAME_STR, numRegions);
+  }
+
   TableName createTableWithRegions(String namespace, int numRegions) throws Exception {
-    final Admin admin = testUtil.getAdmin();
+    return createTableWithRegions(testUtil.getAdmin(), namespace, numRegions);
+  }
+
+  TableName createTableWithRegions(Admin admin, String namespace, int numRegions) throws Exception {
     final TableName tn = TableName.valueOf(
         namespace, testName.getMethodName() + counter.getAndIncrement());
 

vamsi360/hbase-release

Change-Id: I1441be386fc85634a72bc7e571c6c6b3af20e5d4

hbase-server/src/test/java/org/apache/hadoop/hbase/quotas/SpaceQuotaHelperForTests.java
@@ -110,6 +110,18 @@ public class SpaceQuotaHelperForTests {
     }
   }
 
+  QuotaSettings getTableSpaceQuota(Connection conn, TableName tn) throws IOException {
+    try (QuotaRetriever scanner = QuotaRetriever.open(
+        conn.getConfiguration(), new QuotaFilter().setTableFilter(tn.getNameAsString()))) {
+      for (QuotaSettings setting : scanner) {
+        if (setting.getTableName().equals(tn) && setting.getQuotaType() == QuotaType.SPACE) {
+          return setting;
+        }
+      }
+      return null;
+    }
+  }
+
   /**
    * Waits 30seconds for the HBase quota table to exist.
    */
@@ -130,7 +142,10 @@ public class SpaceQuotaHelperForTests {
   }
 
   void writeData(TableName tn, long sizeInBytes) throws IOException {
-    final Connection conn = testUtil.getConnection();
+    writeData(testUtil.getConnection(), tn, sizeInBytes);
+  }
+
+  void writeData(Connection conn, TableName tn, long sizeInBytes) throws IOException {
     final Table table = conn.getTable(tn);
     try {
       List<Put> updates = new ArrayList<>();
@@ -226,8 +241,16 @@ public class SpaceQuotaHelperForTests {
     return createTableWithRegions(NamespaceDescriptor.DEFAULT_NAMESPACE_NAME_STR, numRegions);
   }
 
+  TableName createTableWithRegions(Admin admin, int numRegions) throws Exception {
+    return createTableWithRegions(
+        testUtil.getHBaseAdmin(), NamespaceDescriptor.DEFAULT_NAMESPACE_NAME_STR, numRegions);
+  }
+
   TableName createTableWithRegions(String namespace, int numRegions) throws Exception {
-    final Admin admin = testUtil.getHBaseAdmin();
+    return createTableWithRegions(testUtil.getHBaseAdmin(), namespace, numRegions);
+  }
+
+  TableName createTableWithRegions(Admin admin, String namespace, int numRegions) throws Exception {
     final TableName tn = TableName.valueOf(
         namespace, testName.getMethodName() + counter.getAndIncrement());
 