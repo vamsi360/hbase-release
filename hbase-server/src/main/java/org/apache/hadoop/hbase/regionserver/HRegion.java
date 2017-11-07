d35a441214c92659ea85bde355886a08135af32f
Csaba Skrabak <cskrabak@hortonworks.com>
2017-11-07 09:59:56 +0100

    BUG-91237. HBASE-15095 isReturnResult=false on fast path in branch-1.1 and branch-1.0 is not respected
    (cherry picked from commit 9547d4efbd)
    
    Change-Id: Id20bf5fa9c7ecf06f6c34d40dfe037a9d7f0ee22

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -7238,7 +7238,7 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
       try {
         if (this.coprocessorHost != null) {
           Result r = this.coprocessorHost.preIncrementAfterRowLock(increment);
-          if (r != null) return r;
+          if (r != null) return increment.isReturnResults() ? r : null;
         }
         // Process increments a Store/family at a time.
         long now = EnvironmentEdgeManager.currentTime();
@@ -7312,7 +7312,7 @@ public class HRegion implements HeapSize, PropagatingConfigurationObserver, Regi
     }
     // Request a cache flush.  Do it outside update lock.
     if (isFlushSize(this.addAndGetGlobalMemstoreSize(accumulatedResultSize))) requestFlush();
-    return Result.create(allKVs);
+    return increment.isReturnResults() ? Result.create(allKVs) : null;
   }
 
   private Result slowButConsistentIncrement(Increment increment, long nonceGroup, long nonce)
