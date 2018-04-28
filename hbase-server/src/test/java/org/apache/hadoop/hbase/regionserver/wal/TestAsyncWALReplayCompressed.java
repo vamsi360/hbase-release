fc0aa4af82080e234f7146b660a718ace3e3a9e1
tedyu <yuzhihong@gmail.com>
2018-04-28 09:39:04 -0700

    Disable TestAsyncWALReplayCompressed due to unstable async FSWal
    Change-Id: I7be36dc3443fdb3cc517b4df11eae3d7be6ba725

--- hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestAsyncWALReplayCompressed.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/regionserver/wal/TestAsyncWALReplayCompressed.java
@@ -24,9 +24,11 @@ import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.testclassification.RegionServerTests;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 
 @Category({ RegionServerTests.class, MediumTests.class })
+@Ignore
 public class TestAsyncWALReplayCompressed extends TestAsyncWALReplay {
 
   @ClassRule
