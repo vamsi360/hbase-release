apache/hbase

HBASE-21242 [amv2] Miscellaneous minor log and assign procedure create improvements
For RIT Duration, do better than print ms/seconds. Remove redundant UI
column dedicated to duration when we log it in the status field too.

Make bypass log at INFO level.

Make it so on complete of subprocedure, we note count of outstanding
siblings so we have a clue how much further the parent has to go before
it is done (Helpful when hundreds of servers doing SCP).

Have the SCP run the AP preflight check before creating an AP; saves
creation of thousands of APs during fixup.

Don't log tablename three times when reporting remote call failed.

If lock is held already, note who has it. Also log after we get lock
or if we have to wait rather than log on entrance though we may
later have to wait (or we may have just picked up the lock).

Signed-off-by: Mike Drob <mdrob@apache.org>

hbase-procedure/src/main/java/org/apache/hadoop/hbase/procedure2/ProcedureExecutor.java
@@ -1015,21 +1015,21 @@ public class ProcedureExecutor<TEnvironment> {
       return false;
     }
 
-    LOG.debug("Begin bypass {} with lockWait={}, override={}, recursive={}",
+    LOG.info("Begin bypass {} with lockWait={}, override={}, recursive={}",
         procedure, lockWait, override, recursive);
     IdLock.Entry lockEntry = procExecutionLock.tryLockEntry(procedure.getProcId(), lockWait);
     if (lockEntry == null && !override) {
-      LOG.debug("Waited {} ms, but {} is still running, skipping bypass with force={}",
+      LOG.info("Waited {} ms, but {} is still running, skipping bypass with override={}",
           lockWait, procedure, override);
       return false;
     } else if (lockEntry == null) {
-      LOG.debug("Waited {} ms, but {} is still running, begin bypass with force={}",
+      LOG.info("Waited {} ms, but {} is still running, begin bypass with override={}",
           lockWait, procedure, override);
     }
     try {
       // check whether the procedure is already finished
       if (procedure.isFinished()) {
-        LOG.debug("{} is already finished, skipping bypass", procedure);
+        LOG.info("{} is already finished, skipping bypass", procedure);
         return false;
       }
 
@@ -1054,7 +1054,7 @@ public class ProcedureExecutor<TEnvironment> {
               }
             });
         } else {
-          LOG.debug("{} has children, skipping bypass", procedure);
+          LOG.info("{} has children, skipping bypass", procedure);
           return false;
         }
       }
@@ -1063,7 +1063,7 @@ public class ProcedureExecutor<TEnvironment> {
       if (procedure.hasParent() && procedure.getState() != ProcedureState.RUNNABLE
           && procedure.getState() != ProcedureState.WAITING
           && procedure.getState() != ProcedureState.WAITING_TIMEOUT) {
-        LOG.debug("Bypassing procedures in RUNNABLE, WAITING and WAITING_TIMEOUT states "
+        LOG.info("Bypassing procedures in RUNNABLE, WAITING and WAITING_TIMEOUT states "
                 + "(with no parent), {}",
             procedure);
         // Question: how is the bypass done here?
@@ -1075,7 +1075,7 @@ public class ProcedureExecutor<TEnvironment> {
       // finished yet
       Procedure current = procedure;
       while (current != null) {
-        LOG.debug("Bypassing {}", current);
+        LOG.info("Bypassing {}", current);
         current.bypass(getEnvironment());
         store.update(procedure);
         long parentID = current.getParentProcId();
@@ -1095,9 +1095,9 @@ public class ProcedureExecutor<TEnvironment> {
       if (lockEntry != null) {
         // add the procedure to run queue,
         scheduler.addFront(procedure);
-        LOG.debug("Bypassing {} and its ancestors successfully, adding to queue", procedure);
+        LOG.info("Bypassing {} and its ancestors successfully, adding to queue", procedure);
       } else {
-        LOG.debug("Bypassing {} and its ancestors successfully, but since it is already running, "
+        LOG.info("Bypassing {} and its ancestors successfully, but since it is already running, "
             + "skipping add to queue", procedure);
       }
       return true;
@@ -1507,9 +1507,17 @@ public class ProcedureExecutor<TEnvironment> {
       procStack.release(proc);
 
       if (proc.isSuccess()) {
-        // update metrics on finishing the procedure
+        // update metrics on finishing the procedure.
         proc.updateMetricsOnFinish(getEnvironment(), proc.elapsedTime(), true);
-        LOG.info("Finished " + proc + " in " + StringUtils.humanTimeDiff(proc.elapsedTime()));
+        // Print out count of outstanding siblings if this procedure has a parent.
+        Procedure<TEnvironment> parent = null;
+        if (proc.hasParent()) {
+          parent = procedures.get(proc.getParentProcId());
+        }
+        LOG.info("Finished {} in {}{}",
+            proc,
+            StringUtils.humanTimeDiff(proc.elapsedTime()),
+            parent != null? (", unfinishedSiblingCount=" + parent.getChildrenLatch()): "");
         // Finalize the procedure state
         if (proc.getProcId() == rootProcId) {
           procedureFinished(proc);

vamsi360/hbase-release

Change-Id: I92fb73ddd9a475389a76f0b385952cde88cb030c

hbase-procedure/src/main/java/org/apache/hadoop/hbase/procedure2/ProcedureExecutor.java
@@ -1054,21 +1054,21 @@ public class ProcedureExecutor<TEnvironment> {
       return false;
     }
 
-    LOG.debug("Begin bypass {} with lockWait={}, override={}, recursive={}",
+    LOG.info("Begin bypass {} with lockWait={}, override={}, recursive={}",
         procedure, lockWait, override, recursive);
     IdLock.Entry lockEntry = procExecutionLock.tryLockEntry(procedure.getProcId(), lockWait);
     if (lockEntry == null && !override) {
-      LOG.debug("Waited {} ms, but {} is still running, skipping bypass with force={}",
+      LOG.info("Waited {} ms, but {} is still running, skipping bypass with force={}",
           lockWait, procedure, override);
       return false;
     } else if (lockEntry == null) {
-      LOG.debug("Waited {} ms, but {} is still running, begin bypass with force={}",
+      LOG.info("Waited {} ms, but {} is still running, begin bypass with force={}",
           lockWait, procedure, override);
     }
     try {
       // check whether the procedure is already finished
       if (procedure.isFinished()) {
-        LOG.debug("{} is already finished, skipping bypass", procedure);
+        LOG.info("{} is already finished, skipping bypass", procedure);
         return false;
       }
 
@@ -1093,7 +1093,7 @@ public class ProcedureExecutor<TEnvironment> {
               }
             });
         } else {
-          LOG.debug("{} has children, skipping bypass", procedure);
+          LOG.info("{} has children, skipping bypass", procedure);
           return false;
         }
       }
@@ -1102,7 +1102,7 @@ public class ProcedureExecutor<TEnvironment> {
       if (procedure.hasParent() && procedure.getState() != ProcedureState.RUNNABLE
           && procedure.getState() != ProcedureState.WAITING
           && procedure.getState() != ProcedureState.WAITING_TIMEOUT) {
-        LOG.debug("Bypassing procedures in RUNNABLE, WAITING and WAITING_TIMEOUT states "
+        LOG.info("Bypassing procedures in RUNNABLE, WAITING and WAITING_TIMEOUT states "
                 + "(with no parent), {}",
             procedure);
         // Question: how is the bypass done here?
@@ -1114,7 +1114,7 @@ public class ProcedureExecutor<TEnvironment> {
       // finished yet
       Procedure<TEnvironment> current = procedure;
       while (current != null) {
-        LOG.debug("Bypassing {}", current);
+        LOG.info("Bypassing {}", current);
         current.bypass(getEnvironment());
         store.update(procedure);
         long parentID = current.getParentProcId();
@@ -1134,9 +1134,9 @@ public class ProcedureExecutor<TEnvironment> {
       if (lockEntry != null) {
         // add the procedure to run queue,
         scheduler.addFront(procedure);
-        LOG.debug("Bypassing {} and its ancestors successfully, adding to queue", procedure);
+        LOG.info("Bypassing {} and its ancestors successfully, adding to queue", procedure);
       } else {
-        LOG.debug("Bypassing {} and its ancestors successfully, but since it is already running, "
+        LOG.info("Bypassing {} and its ancestors successfully, but since it is already running, "
             + "skipping add to queue", procedure);
       }
       return true;
@@ -1546,9 +1546,17 @@ public class ProcedureExecutor<TEnvironment> {
       procStack.release(proc);
 
       if (proc.isSuccess()) {
-        // update metrics on finishing the procedure
+        // update metrics on finishing the procedure.
         proc.updateMetricsOnFinish(getEnvironment(), proc.elapsedTime(), true);
-        LOG.info("Finished " + proc + " in " + StringUtils.humanTimeDiff(proc.elapsedTime()));
+        // Print out count of outstanding siblings if this procedure has a parent.
+        Procedure<TEnvironment> parent = null;
+        if (proc.hasParent()) {
+          parent = procedures.get(proc.getParentProcId());
+        }
+        LOG.info("Finished {} in {}{}",
+            proc,
+            StringUtils.humanTimeDiff(proc.elapsedTime()),
+            parent != null? (", unfinishedSiblingCount=" + parent.getChildrenLatch()): "");
         // Finalize the procedure state
         if (proc.getProcId() == rootProcId) {
           procedureFinished(proc);